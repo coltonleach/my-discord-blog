cf9453c99f159dd6b0be7f12682474e74376aeee
Colton Leach <leach.colton@hotmail.com>
2022-11-07 12:32:05 -0600

    changed url for api call

--- src/App.js
+++ src/App.js
@@ -11,46 +11,45 @@ function App() {
       title: 'the blogs got lost',
       body: [
         `Hi, welcome to my website! If you're seeing this, it means the blogs are on their way and should be here shortly!`,
-        `Have a great day 😁`
+        `Have a great day 😁`,
       ],
       date: 'March 15, 2022',
-      section: 'home'
-    }
+      section: 'home',
+    },
   ])
-	const [home, setHome] = useState(null)
-	const [blog, setBlog] = useState(null)
+  const [home, setHome] = useState(null)
+  const [blog, setBlog] = useState(null)
   const [loading, setLoading] = useState(true)
-  
-  const baseUrl = 'https://dry-bayou-39878.herokuapp.com/api/articles'
-  
-  useEffect(()=> {
+
+  const baseUrl = 'https://confused-school-uniform-bass.cyclic.app/api/articles'
+
+  useEffect(() => {
     fetch(baseUrl)
-      .then(res => res.json())
-      .then(data => {
-        setHome(data.filter(article => article.section === 'home'))
-        setBlog(data.filter(article => article.section === 'blog'))
+      .then((res) => res.json())
+      .then((data) => {
+        setHome(data.filter((article) => article.section === 'home'))
+        setBlog(data.filter((article) => article.section === 'blog'))
         setLoading(false)
       })
   }, [])
 
-	return (
+  return (
     <>
-			<Router>
-				<NavBar />
-				{loading
-        ? <Container articles={initialScreen} />
-        :
+      <Router>
+        <NavBar />
+        {loading ? (
+          <Container articles={initialScreen} />
+        ) : (
           <Switch>
-					<Route exact path="/">
-						{home && <Container articles={home} />}
-					</Route>
-					<Route path="/blog">
-						{blog && <Container articles={blog} />}
-					</Route>
-				</Switch>}
-			</Router>
-		</>
-	)
+            <Route exact path='/'>
+              {home && <Container articles={home} />}
+            </Route>
+            <Route path='/blog'>{blog && <Container articles={blog} />}</Route>
+          </Switch>
+        )}
+      </Router>
+    </>
+  )
 }
 
 export default App
